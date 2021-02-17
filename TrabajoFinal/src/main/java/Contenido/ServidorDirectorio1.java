c7403fc134ada35c178c1b7b16ed23c8436de2fe
dbm349 <62100143+dbm349@users.noreply.github.com>
2021-02-17 18:44:22 -0300

    agrego tareas del servidor de directorios

--- TrabajoFinal/src/main/java/Contenido/ServidorDirectorio1.java
+++ TrabajoFinal/src/main/java/Contenido/ServidorDirectorio1.java
@@ -1,5 +1,10 @@
 package Contenido;
 
+//El servidor de directorios debe poder encolar las peticiones de lo usuarios que desean realizar alguna operación sobre el mismo archivo, puede ser una cola por archivo
+//El servidor de directorios debe conocer a los archivod que contiene
+//El servidor de directorios1 debe poder actualizar al servidor de directorios2 que actuará cono servidor de respaldo, ya sea por sobrecarga de consultas o caida del servidor primario.
+
+
 public class ServidorDirectorio1 {
 
 }
