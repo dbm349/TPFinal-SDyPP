64cc39f5959576b709d820d4d916fd145e597f20
dbm349 <62100143+dbm349@users.noreply.github.com>
2021-02-17 18:45:05 -0300

    Update ServidorDirectorio1.java

--- TrabajoFinal/src/main/java/Contenido/ServidorDirectorio1.java
+++ TrabajoFinal/src/main/java/Contenido/ServidorDirectorio1.java
@@ -3,7 +3,7 @@ package Contenido;
 //El servidor de directorios debe poder encolar las peticiones de lo usuarios que desean realizar alguna operación sobre el mismo archivo, puede ser una cola por archivo
 //El servidor de directorios debe conocer a los archivod que contiene
 //El servidor de directorios1 debe poder actualizar al servidor de directorios2 que actuará cono servidor de respaldo, ya sea por sobrecarga de consultas o caida del servidor primario.
-
+//El servidor de directorios debe poder crear un nuevo directorio
 
 public class ServidorDirectorio1 {
 
