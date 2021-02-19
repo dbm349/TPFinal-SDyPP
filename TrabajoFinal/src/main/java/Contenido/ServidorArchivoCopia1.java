5c6dad8d65e23aa120284a7ea7d3a05343fa90d6
dbm349 <62100143+dbm349@users.noreply.github.com>
2021-02-19 17:17:58 -0300

    agrego tareas del servidor de backup

--- TrabajoFinal/src/main/java/Contenido/ServidorArchivoCopia1.java
+++ TrabajoFinal/src/main/java/Contenido/ServidorArchivoCopia1.java
@@ -1,5 +1,9 @@
 package Contenido;
 
+//Este servidor debe contener los archivos que se encuentran en el servidor de archivos1
+//Tenemos que establecer cada cuanto tiempo hacemos el backup
+//Este servidor debe poder brindar su contenido al servidor de archivos1 cuando lo solicite
+
 /**
  * Hello world!
  *
