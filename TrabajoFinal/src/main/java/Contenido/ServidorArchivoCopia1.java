8e978998da1e4bc4de799f884291466c552c576d
dbm349 <62100143+dbm349@users.noreply.github.com>
2021-12-20 19:51:01 -0300

    Modifico tareas

--- TrabajoFinal/src/main/java/Contenido/ServidorArchivoCopia1.java
+++ TrabajoFinal/src/main/java/Contenido/ServidorArchivoCopia1.java
@@ -1,8 +1,7 @@
 package Contenido;
 
 //Este servidor debe contener los archivos que se encuentran en el servidor de archivos1
-//Tenemos que establecer cada cuanto tiempo hacemos el backup
-//Este servidor debe poder brindar su contenido al servidor de archivos1 cuando lo solicite
+//Este servidor debe poder realizar operaciones de lectura
 
 /**
  * Hello world!
