dc532fe5d7c3d21363009dda4ecaf3b5f65c8057
dbm349 <62100143+dbm349@users.noreply.github.com>
2021-12-23 17:11:49 -0300

    Cambios en thread

--- TrabajoFinal/src/main/java/Contenido/ThreadServidorArchivo1.java
+++ TrabajoFinal/src/main/java/Contenido/ThreadServidorArchivo1.java
@@ -16,7 +16,62 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MDC;
 
-public class ThreadServidorArchivo1 implements Runnable{
+
+public class ThreadServidorArchivo1 implements Runnable {
+	Socket cliente;
+	private ArrayList<String> colaDeMensajes;
+	
+	public HiloServidor(Socket cliente,ArrayList<String> colaDeMensajes) {
+		this.cliente=cliente;
+		this.colaDeMensajes=colaDeMensajes;
+	}
+	
+	public void run() {
+		try {
+			BufferedReader entrada = new BufferedReader (new InputStreamReader(cliente.getInputStream()));
+			PrintStream salida = new PrintStream (this.cliente.getOutputStream(),true);
+			
+			while(!cliente.isClosed()) {
+				String opcion = entrada.readLine();
+					
+				if (opcion.substring(opcion.indexOf("|")+1, opcion.length()).equals("SEND")) {
+					
+					//Leo del cliente y guardo en la cola
+					String mensEnv = entrada.readLine();
+					colaDeMensajes.add(mensEnv);
+					
+				}else if (opcion.substring(opcion.indexOf("|")+1, opcion.length()).equals("RECV")) {
+					//Extraigo el ID del cliente que realiza la peticion
+					String srcId = opcion.substring(0, opcion.indexOf("|"));
+					if (!this.colaDeMensajes.isEmpty()) {
+						for (String str: colaDeMensajes) {
+							//Recorro la cola y comparo el ID del cliente con los destinos de los mensajes
+							//Envio mensaje al cliente si es igual
+							String destinoID = str.substring(0, str.indexOf("|"));
+							String MsgSEND = str.substring(str.indexOf("|")+1, str.length());
+							if (destinoID.equals(srcId)) {
+								salida.println(MsgSEND);
+							}
+							System.out.println("Mensaje leido: "+MsgSEND);
+						}
+					}
+					String Final = "No.mas.mensajes.";
+					salida.println(Final);	
+					System.out.println("Fin de mensajes");
+				}
+			}
+			this.cliente.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
+
+
+
+
+
+/*public class ThreadServidorArchivo1 implements Runnable{
 	
 	private final Logger log = LoggerFactory.getLogger(ThreadServidorArchivo1.class);
 	private PrintWriter salida;
@@ -120,4 +175,4 @@ public class ThreadServidorArchivo1 implements Runnable{
 				
 			};
 
-}
+}*/
