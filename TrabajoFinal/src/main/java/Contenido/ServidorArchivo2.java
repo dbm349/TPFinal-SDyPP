d72cc85e8f6d52a30da06c5914d48323d2cd2da7
dbm349 <62100143+dbm349@users.noreply.github.com>
2021-12-23 17:14:42 -0300

    Creo conexión del servidor de archivo 2

--- TrabajoFinal/src/main/java/Contenido/ServidorArchivo2.java
+++ TrabajoFinal/src/main/java/Contenido/ServidorArchivo2.java
@@ -1,5 +1,54 @@
 package Contenido;
 
-public class ServidorArchivo2 {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.slf4j.MDC;
+
+//El servidor de archivos debe poder modificar un archivo, verificando el tamaño del mismo
+//El servidor de archivos debe poder borrar un archivo
+//El servidor de archivos debe poder crear un archivo, verificando el tamaño del mismo
+//El servidor de archivos debe poder llamar al servidor de archivos secundario si se encuentra con muchas peticiones 
+//(El servidor de archivos debe realizar una copia del mismo al terminar de realizar alguna de las operaciones anteriores??)
+
+
+
+
+public class ServidorArchivo2 implements Runnable {
+	int port;
+	private ArrayList<String> colaDeMensajes;
+	
+	public ServidorArchivo2(int port) {
+		this.port=port;
+		this.colaDeMensajes= new ArrayList<String>();
+		this.StartServer();
+	}
+
+	public void StartServer() {
+		try {
+			ServerSocket ss = new ServerSocket(port);
+			//Servidor Escuchando puerto ingresado
+			System.out.println("---Servidor iniciado, escuchando en puerto "+port+"---");
+			while (true) {
+				Socket cliente = ss.accept();
+				System.out.println("Cliente conectado: "+cliente.getInetAddress().getCanonicalHostName()+" : "+cliente.getPort());
+				HiloServidor hs = new HiloServidor(cliente,colaDeMensajes);
+				Thread HSthread = new Thread(hs);
+				HSthread.start();
+			}
+		} catch (IOException e) {
+			System.out.println("Puerto en uso");
+		}
+	}
+	
+	public static void main(String[] args) {
+		//Para el ejemplo utilizo puerto 6000
+		ServidorArchivo2 servidor = new ServidorArchivo2(6001);
+	}
 
 }
+
