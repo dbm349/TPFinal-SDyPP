01845ba62eb910131b027c9755efcbd21648bd9c
dbm349 <delmore97@hotmail.com>
2022-01-07 17:05:43 -0300

    leemos lo que el usuario desea escribir por consola

--- TrabajoFinal/src/main/java/Contenido/ficheros_directorios/Escribiendo.java
+++ TrabajoFinal/src/main/java/Contenido/ficheros_directorios/Escribiendo.java
@@ -1,13 +1,18 @@
 package ficheros_directorios;
 
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Escribiendo {
 
-    public void escribir(String ruta_archivo){
+    public void escribir(String ruta_archivo) throws IOException {
 
-        String frase= "Esto es un ejemplo. Espero que funcione bien";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Ingrese el texto con el que desea crear el archivo");
+        String frase = br.readLine();
+        //String frase= "Esto es un ejemplo. Espero que funcione bien";
 
         try{
 
@@ -18,6 +23,8 @@ public class Escribiendo {
             }
 
             escritura.close();
+            System.out.println("Archivo creado correctamente");
+
 
         }catch(IOException e){
 
